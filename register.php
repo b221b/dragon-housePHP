a25b94628b9df8e488ad069d566600606bc4d88c
b221b <titarenkomiroslav61@gmail.com>
2024-06-25 17:41:08 +0300

    ура, наконец нельзя регаться с таким же ником

--- register.php
+++ register.php
@@ -8,11 +8,12 @@ $avatar = $_FILES['avatar']['name'];
 $birthday = $_POST['birthday'];
 $password = $_POST['password'];
 
-// Создаем запрос на добавление данных в таблицу Users
-$query = "INSERT INTO Users (Login, Mail, Avatar, Birthday, Password) VALUES ('$login', '$mail', '$avatar', '$birthday', '$password')";
+// Check if the username already exists in the database
+$query = "SELECT * FROM Users WHERE Login = '$login'";
+$result = mysqli_query($conn, $query);
 
-// Выполняем запрос
-if (mysqli_query($conn, $query)) {
+if (mysqli_num_rows($result) > 0) {
+    // Username already exists, display error message
 ?>
     <html>
 
@@ -39,45 +40,85 @@ if (mysqli_query($conn, $query)) {
     </head>
 
     <body>
-        <img src="images/loadleave-leave.gif" class="loader" alt="Вы успешно зарегистрировались!">
-        <meta http-equiv="refresh" content="<?php echo rand(2, 5); ?>; url=Index.php">
+        <img src="images/loadleave-leave.gif" class="loader" alt="Ошибка регистрации">
+        <?php echo "Пользователь с именем $login уже существует"; ?>
+        <meta http-equiv="refresh" content="<?php echo rand(4, 5); ?>; url=Index.php">
     </body>
 
     </html>
-<?php
+    <?php
 } else {
-?>
-    <html>
+    // Create a query to insert the data into the database
+    $query = "INSERT INTO Users (Login, Mail, Avatar, Birthday, Password) VALUES ('$login', '$mail', '$avatar', '$birthday', '$password')";
 
-    <head>
-        <style>
-            body {
-                margin: 0;
-                height: 100vh;
-                display: flex;
-                justify-content: center;
-                align-items: center;
-                background-color: #f0f0f0;
-            }
+    // Execute the query
+    if (mysqli_query($conn, $query)) {
+    ?>
+        <html>
 
-            .loader {
-                width: 50%;
-                /* адаптивный размер */
-                max-width: 200px;
-                /* максимальный размер */
-                height: auto;
-                margin: 0 auto;
-            }
-        </style>
-    </head>
+        <head>
+            <style>
+                body {
+                    margin: 0;
+                    height: 100vh;
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    background-color: #f0f0f0;
+                }
 
-    <body>
-        <img src="images/ОшибкаРегистрации.jpg" class="loader" alt="Ошибка регистрации">
-        <?php echo "Ошибка регистрации: " . mysqli_error($conn); ?>
-    </body>
+                .loader {
+                    width: 50%;
+                    /* адаптивный размер */
+                    max-width: 200px;
+                    /* максимальный размер */
+                    height: auto;
+                    margin: 0 auto;
+                }
+            </style>
+        </head>
 
-    </html>
+        <body>
+            <img src="images/loadleave-leave.gif" class="loader" alt="Вы успешно зарегистрировались!">
+            <meta http-equiv="refresh" content="<?php echo rand(2, 5); ?>; url=Index.php">
+        </body>
+
+        </html>
+    <?php
+    } else {
+    ?>
+        <html>
+
+        <head>
+            <style>
+                body {
+                    margin: 0;
+                    height: 100vh;
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    background-color: #f0f0f0;
+                }
+
+                .loader {
+                    width: 50%;
+                    /* адаптивный размер */
+                    max-width: 200px;
+                    /* максимальный размер */
+                    height: auto;
+                    margin: 0 auto;
+                }
+            </style>
+        </head>
+
+        <body>
+            <img src="images/ОшибкаРегистрации.jpg" class="loader" alt="Ошибка регистрации">
+            <?php echo "Ошибка регистрации: " . mysqli_error($conn); ?>
+        </body>
+
+        </html>
 <?php
+    }
 }
 
 // Закрываем соединение
