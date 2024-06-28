e79eeecc6215d4248e7a3c438e22cfb2608f7156
b221b <titarenkomiroslav61@gmail.com>
2024-06-28 15:27:32 +0300

    почистил код в файле

--- register.php
+++ register.php
@@ -8,119 +8,48 @@ $avatar = $_FILES['avatar']['name'];
 $birthday = $_POST['birthday'];
 $password = $_POST['password'];
 
-// Check if the username already exists in the database
+// Проверяем, существует ли уже пользователь с таким логином
 $query = "SELECT * FROM Users WHERE Login = '$login'";
 $result = mysqli_query($conn, $query);
 
 if (mysqli_num_rows($result) > 0) {
-    // Username already exists, display error message
-?>
-    <html>
-
-    <head>
-        <style>
-            body {
-                margin: 0;
-                height: 100vh;
-                display: flex;
-                justify-content: center;
-                align-items: center;
-                background-color: #f0f0f0;
-            }
-
-            .loader {
-                width: 50%;
-                /* адаптивный размер */
-                max-width: 200px;
-                /* максимальный размер */
-                height: auto;
-                margin: 0 auto;
-            }
-        </style>
-    </head>
-
-    <body>
-        <img src="images/loadleave-leave.gif" class="loader" alt="Ошибка регистрации">
-        <?php echo "Пользователь с именем $login уже существует"; ?>
-        <meta http-equiv="refresh" content="<?php echo rand(4, 5); ?>; url=Index.php">
-    </body>
-
-    </html>
-    <?php
+    // Логин уже существует, выводим сообщение об ошибке
+    displayError("Пользователь с именем $login уже существует");
 } else {
-    // Create a query to insert the data into the database
+    // Создаем запрос на вставку данных в БД
     $query = "INSERT INTO Users (Login, Mail, Avatar, Birthday, Password) VALUES ('$login', '$mail', '$avatar', '$birthday', '$password')";
 
-    // Execute the query
+    // Выполняем запрос
     if (mysqli_query($conn, $query)) {
-    ?>
-        <html>
-
-        <head>
-            <style>
-                body {
-                    margin: 0;
-                    height: 100vh;
-                    display: flex;
-                    justify-content: center;
-                    align-items: center;
-                    background-color: #f0f0f0;
-                }
-
-                .loader {
-                    width: 50%;
-                    /* адаптивный размер */
-                    max-width: 200px;
-                    /* максимальный размер */
-                    height: auto;
-                    margin: 0 auto;
-                }
-            </style>
-        </head>
-
-        <body>
-            <img src="images/loadleave-leave.gif" class="loader" alt="Вы успешно зарегистрировались!">
-            <meta http-equiv="refresh" content="<?php echo rand(2, 5); ?>; url=Index.php">
-        </body>
-
-        </html>
-    <?php
+        displaySuccess();
     } else {
-    ?>
-        <html>
-
-        <head>
-            <style>
-                body {
-                    margin: 0;
-                    height: 100vh;
-                    display: flex;
-                    justify-content: center;
-                    align-items: center;
-                    background-color: #f0f0f0;
-                }
-
-                .loader {
-                    width: 50%;
-                    /* адаптивный размер */
-                    max-width: 200px;
-                    /* максимальный размер */
-                    height: auto;
-                    margin: 0 auto;
-                }
-            </style>
-        </head>
-
-        <body>
-            <img src="images/ОшибкаРегистрации.jpg" class="loader" alt="Ошибка регистрации">
-            <?php echo "Ошибка регистрации: " . mysqli_error($conn); ?>
-        </body>
-
-        </html>
-<?php
+        displayError("Ошибка регистрации: " . mysqli_error($conn));
     }
 }
 
 // Закрываем соединение
 mysqli_close($conn);
-?>
+
+// Функции для отображения сообщений
+function displayError($message)
+{
+    echo "<html><head><style>" . getStyles() . "</style></head><body>";
+    echo "<img src='images/loadleave-leave.gif' class='loader' alt='Ошибка регистрации'>";
+    echo $message;
+    echo "<meta http-equiv='refresh' content='" . rand(4, 5) . "; url=Index.php'>";
+    echo "</body></html>";
+}
+
+function displaySuccess()
+{
+    echo "<html><head><style>" . getStyles() . "</style></head><body>";
+    echo "<img src='images/loadleave-leave.gif' class='loader' alt='Вы успешно зарегистрировались!'>";
+    echo "<meta http-equiv='refresh' content='" . rand(2, 5) . "; url=Index.php'>";
+    echo "</body></html>";
+}
+
+function getStyles()
+{
+    return "body { margin: 0; height: 100vh; display: flex; justify-content: center; align-items: center; background-color: #f0f0f0; }
+            .loader { width: 50%; max-width: 200px; height: auto; margin: 0 auto; }";
+}
